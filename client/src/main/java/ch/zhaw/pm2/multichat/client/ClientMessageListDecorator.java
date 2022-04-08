5365b66b3d83a77b967bd2ab63d2141c727065e0
jasarard <jasarard@students.zhaw.ch>
2022-04-08 10:42:06 +0200

    added original methods of ClientMessageList to ClientMessageListDecorator #5

--- client/src/main/java/ch/zhaw/pm2/multichat/client/ClientMessageListDecorator.java
+++ client/src/main/java/ch/zhaw/pm2/multichat/client/ClientMessageListDecorator.java
@@ -3,6 +3,8 @@ package ch.zhaw.pm2.multichat.client;
 import java.util.ArrayList;
 import java.util.List;
 
+import ch.zhaw.pm2.multichat.client.ClientMessageList.MessageType;
+
 /**
  * Adds observable functionality to one ClientMessageList-object.
  * The decorator uses the original methods of the ClientMessageList-object.
@@ -25,6 +27,16 @@ public class ClientMessageListDecorator implements IsObservable {
     @Override
     public void removeListener(IsObserver observer) {
         listener.remove(observer);
+    }
+
+    public void addMessage(MessageType type, String sender, String receiver, String message) {
+        clientMessageList.addMessage(type, sender, receiver, message);
+        informListener();
+    }
+
+    public void writeFilteredMessages(String filter) { 
+        clientMessageList.writeFilteredMessages(filter);
+        informListener();
     }
     
     private void informListener() {
