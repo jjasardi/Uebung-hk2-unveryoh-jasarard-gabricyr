2972af5048095ad1b47c85e74fb2d834b0eb0357
jasarard <jasarard@students.zhaw.ch>
2022-04-09 23:11:17 +0200

    made data fields final and added getters in Message #16

--- client/src/main/java/ch/zhaw/pm2/multichat/Message.java
+++ client/src/main/java/ch/zhaw/pm2/multichat/Message.java
@@ -1,16 +1,32 @@
 package ch.zhaw.pm2.multichat;
 
 public class Message {
-    private MessageType type;
-    private String sender;
-    private String receiver;
-    private String text;
+    private final MessageType type;
+    private final String sender;
+    private final String receiver;
+    private final String text;
 
     public Message(MessageType type, String sender, String receiver, String text) {
         this.type = type;
         this.sender = sender;
         this.receiver = receiver;
         this.text = text;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public String getText() {
+        return text;
     }
 
     // TODO move enum in new config file
